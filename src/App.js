3504f0c78e1102b2f404d1173a769032f83fcf46
Victor <droidvhm7@gmail.com>
2021-03-22 19:24:45 -0300

    code push

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import Header from './components/Header';
+
+import './App.css';
+
+function App() {
+  const [projects, setProjects] = useState([
+    'Desenvolvimento de app',
+    'Front-end web',
+  ]);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
+}
+
+export default App;
